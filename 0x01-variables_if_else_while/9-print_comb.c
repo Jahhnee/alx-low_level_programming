9d73c0323e61407f14734e685f1aec59d4c06d8f
Jahhnee <abakpaobekpajohn@gmail.com>
2022-03-11 12:49:06 -0800

    Possible combination of single numbers

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,22 +1,26 @@
 #include <stdio.h>
 /**
  * main - main block
- * Description: Print all numbers of base64 in lowercase.
- * You can only use `putchar`, and only 3 times.
+ * Description: Print all possible combinations fo single-digit numbers.
+ * Numbers must be separated by commas and a space.
+ * You can only use `putchar` to print to console
+ * You can only use `putchar` up to four times.
+ * You are not allowed to use any variable of type `char`.
  * Return: 0
  */
 int main(void)
 {
-	char c;
-	int i;
+	int i = 0;
 
-	for (i = 0; i < 10; i++)
+	while (i < 10)
 	{
 		putchar(i + '0');
-	}
-	for (c = 'a'; c < 'g'; c++)
-	{
-		putchar(c);
+		if (i < 9)
+		{
+			putchar(44);
+			putchar(32);
+		}
+		i++;
 	}
 	putchar('\n');
 
